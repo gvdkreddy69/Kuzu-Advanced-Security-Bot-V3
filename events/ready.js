9813c4aef5acde65b0f9e68f3e403a3f6e434656
G V D K REDDY <121618501+gvdkreddy69@users.noreply.github.com>
2024-04-15 14:22:59 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   client.on("ready", async () => {
-    console.log(`Kronix | ${client.user.tag} Has Logged In!\nKronix | Emoji Server: https://discord.gg/fU7YdD2dQ6\nKronix | Subscribe to Team Kronix.`)
+    console.log(`Kronix | ${client.user.tag} Has Logged In!\nKronix | Emoji Server: https://discord.gg/SvBZThMyUf\nKronix | Subscribe to Team Kronix.`)
     client.user.setPresence({
       activities: [{
         name: `${client.config.prefix}help`,
