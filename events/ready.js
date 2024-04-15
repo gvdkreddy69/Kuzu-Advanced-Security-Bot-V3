cbc144f69022d655bb72218a842b5db1848f2d7c
G V D K REDDY <121618501+gvdkreddy69@users.noreply.github.com>
2024-04-15 13:22:17 +0530

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,13 @@
+module.exports = async (client) => {
+  client.on("ready", async () => {
+    console.log(`${client.user.tag} Has Logged In!\nEmoji Server: https://discord.gg/fU7YdD2dQ6\nSubscribe to The Extremez Coder`)
+    client.user.setPresence({
+      activities: [{
+        name: `${client.config.prefix}help`,
+        type: "STREAMING",
+        url: "https://www.youtube.com/watch?v=jFlphUBZpo4",
+      }],
+      status: "online"
+    });
+  });
+};
