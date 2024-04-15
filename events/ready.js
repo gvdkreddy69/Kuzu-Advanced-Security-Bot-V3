1e981efd514ff08a8170ecc2ecc12351d1fbedbb
G V D K REDDY <121618501+gvdkreddy69@users.noreply.github.com>
2024-04-15 13:29:08 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,11 @@
 module.exports = async (client) => {
   client.on("ready", async () => {
-    console.log(`${client.user.tag} Has Logged In!\nEmoji Server: https://discord.gg/fU7YdD2dQ6\nSubscribe to The Extremez Coder`)
+    console.log(`Kronix | ${client.user.tag} Has Logged In!\nKronix | Emoji Server: https://discord.gg/fU7YdD2dQ6\nKronix | Subscribe to Team Kronix.`)
     client.user.setPresence({
       activities: [{
         name: `${client.config.prefix}help`,
         type: "STREAMING",
-        url: "https://www.youtube.com/watch?v=jFlphUBZpo4",
+        url: "https://www.discord.gg/teamkronix",
       }],
       status: "online"
     });
